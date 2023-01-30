8804833c7592e7f876d018c9c958ce16865f87ca
Ragnar Olafsson <ragnar.orn@gmail.com>
2023-01-30 20:10:25 +0000

    initial commit for learning threading in python

---
+++ python-parallel/main.py
@@ -0,0 +1,19 @@
+import time
+
+def calculate_sum_squares(n):
+    sum_of_square = 0
+
+    for i in range(n):
+        sum_of_square += i ** 2
+
+    print(sum_of_square)        
+
+def sleep_a_little(seconds):
+   time.sleep(seconds)
+
+def main():
+   ... 
+
+
+if __name__ == "__main__":
+    main()
